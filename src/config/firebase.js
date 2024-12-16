35df5c5d741260df89013a40054a81220ed0e691
MSalim16 <mohamedsalim192002@gmail.com>
2024-12-16 18:58:34 +0000

    fixing firebase security

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA1M3eKM3S9g6zW64OOcfm4w2EwYzhSl9Y",
-  authDomain: "progresspilot-c0e15.firebaseapp.com",
-  projectId: "progresspilot-c0e15",
-  storageBucket: "progresspilot-c0e15.firebasestorage.app",
-  messagingSenderId: "900347606284",
-  appId: "1:900347606284:web:83c2ba191cb925f855c529",
-  measurementId: "G-XWP732LBER"
+  apiKey: "AIzaSyA-AA3ZV1Db6ul7gxBYnlgsK91PxO1AOow",
+  authDomain: "progresspilot-1277a.firebaseapp.com",
+  projectId: "progresspilot-1277a",
+  storageBucket: "progresspilot-1277a.firebasestorage.app",
+  messagingSenderId: "158593835530",
+  appId: "1:158593835530:web:9701992dc00409f346b5f3",
+  measurementId: "G-78JPGDDMRQ"
 };
 
 // Initialize Firebase
